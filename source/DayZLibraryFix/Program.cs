7548068f5361481734fe340014a5464d8de6f067
NiiRoZz <extazziii1000@gmail.com>
2019-07-08 22:25:49 +0200

    Added folder browser instead using parameter

--- source/DayZLibraryFix/Program.cs
+++ source/DayZLibraryFix/Program.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
+using System.Windows.Forms;
 
 namespace DayZLibraryFix
 {
@@ -28,46 +27,52 @@ namespace DayZLibraryFix
 
             return false;
         }
+
+        [STAThread]
         static void Main(string[] args)
         {
-            if ( args.Count() != 1 )
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if ( fbd.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
             {
-                Console.WriteLine("You should have only 1 argument");
-                return;
-            }
+                string pathFolder = fbd.SelectedPath;
 
-            string pathFolder = args[0];
-
-            if ( ! Directory.Exists(pathFolder) )
-            {
-                Console.WriteLine("The path is not a existing folder");
-                return;
-            }
-
-            IEnumerable<string> files = Directory.EnumerateFiles(pathFolder, "*.tml");
-
-            List<XmlDocument> allDocuments = new List<XmlDocument>();
-
-            foreach (string file in files)
-            {
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(file);
-                allDocuments.Add(xmlDoc);
-            }
-
-            foreach( XmlDocument document in allDocuments)
-            {
-                XmlNodeList nodes = document.DocumentElement.SelectNodes("/Library/Template");
-                foreach (XmlNode node in nodes)
+                if (!Directory.Exists(pathFolder))
                 {
-                    string filePath = node.SelectSingleNode("File").InnerText;
+                    Console.WriteLine("The path is not a existing folder");
+                    return;
+                }
 
-                    if ( detectDuplicatePath(allDocuments, filePath, node.SelectSingleNode("Name").InnerText) )
+                IEnumerable<string> files = Directory.EnumerateFiles(pathFolder, "*.tml");
+
+                List<XmlDocument> allDocuments = new List<XmlDocument>();
+
+                foreach (string file in files)
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(file);
+                    allDocuments.Add(xmlDoc);
+                }
+
+                foreach (XmlDocument document in allDocuments)
+                {
+                    XmlNodeList nodes = document.DocumentElement.SelectNodes("/Library/Template");
+                    foreach (XmlNode node in nodes)
                     {
-                        Console.WriteLine("Path duplicated for {0}", filePath);
-                        return;
+                        string filePath = node.SelectSingleNode("File").InnerText;
+
+                        if (detectDuplicatePath(allDocuments, filePath, node.SelectSingleNode("Name").InnerText))
+                        {
+                            Console.WriteLine("Path duplicated for {0}", filePath);
+                            Console.WriteLine("PRESS any key to exit");
+                            Console.ReadKey();
+                            return;
+                        }
                     }
                 }
+
+                Console.WriteLine("All path is valid, PRESS any key to exit");
+                Console.ReadKey();
             }
         }
     }
